5bedf7b964228f8667eb7c55051b89503e6c0b4f
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-02-04 08:40:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5.git /Auto-Filter-V5
+  git clone https://github.com/Jegaa1/url-auto-delete-shortener-bot3.git /url-auto-delete-shortener-bot3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5
+  git clone $UPSTREAM_REPO /url-auto-delete-shortener-bot3
 fi
-cd /Auto-Filter-V5
+cd /url-auto-delete-shortener-bot3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
