2edc1a8045e0033ab58c70aec595af701984ed7f
mirrorbot22 <118037645+mirrorbot22@users.noreply.github.com>
2023-04-21 00:16:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,9 +37,9 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filter:filter@filter.p0kh4il.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "RENAMETR")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutofilterBot')
 
 # Channel Button Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/TR_Discussion')
