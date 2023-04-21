3de21b6eea1dd0d6d37a93e4c754d56ebe7d960f
mirrorbot22 <118037645+mirrorbot22@users.noreply.github.com>
2023-04-21 09:36:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "36dffdkjfdhfdhfdhfdbbfddddddd")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/572318b12f3ac31449ab7.jpg")
+PICS = (environ.get('PICS', "https://telegra.ph/file/572318b12f3ac31449ab7.jpg")
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/94750f782f45f592b823f.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/25e935202c5c9da2d1cc7.jpg")
 NEWGRP = environ.get("NEWGRP", "https://telegra.ph/file/db4c49f28a7a33008ca08.jpg")
